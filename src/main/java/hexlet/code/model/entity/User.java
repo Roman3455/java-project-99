1c737b8315f23156fd8233a4127a973bf5b2bb01
Roman Smirnov <s.roman3455@gmail.com>
2025-03-05 21:45:30 +0300

    Add data connection & user entity

---
+++ src/main/java/hexlet/code/model/entity/User.java
@@ -0,0 +1,58 @@
+package hexlet.code.model.entity;
+
+//import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+//import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+//import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+//import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+public class User implements BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Email
+    @NotEmpty
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    @Size(min = 3)
+    private String password;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    private LocalDate updatedAt;
+
+//    @OneToMany(mappedBy = "assignee", cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+//    private List<Task> tasks;
+}
